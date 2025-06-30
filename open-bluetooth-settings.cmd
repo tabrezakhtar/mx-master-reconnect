50dcfe499410a286d655a7e97d1157b7724a5d39
tabrez <tabrezakhtar@users.noreply.github.com>
2025-06-30 16:03:34 +0100

    Enhance Bluetooth settings script to provide user feedback and automate tab navigation

--- open-bluetooth-settings.cmd
+++ open-bluetooth-settings.cmd
@@ -1,4 +1,5 @@
 @echo off
-start ms-settings:bluetooth
+echo Opening Windows Bluetooth settings...
+powershell -command "Start-Process ms-settings:bluetooth; Start-Sleep -Seconds 1; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{TAB}');"
 echo Windows Bluetooth settings opened. Use your keyboard to add or reconnect your MX Master mouse.
 pause
